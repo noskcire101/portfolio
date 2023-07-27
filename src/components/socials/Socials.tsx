6f80f299fdfa2b2a69c0c43c9a15412624216161
noskcire101 <esernero@gmail.com>
2023-07-27 23:53:15 +0800

    update colors

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -20,14 +20,14 @@ const Socials = () => {
         >
           <a
             href="mailto:esernero@outlook.com"
-            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200 dark:border-gray-600 dark:hover:text-white shadow-sm dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400"
+            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200   shadow-sm  hover:bg-gray-50"
           >
             <SiMicrosoftoutlook className="w-5 h-5" />
           </a>
 
           <a
             href="mailto:esernero@gmail.com"
-            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200 dark:border-gray-600 dark:hover:text-white shadow-sm dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400"
+            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200   shadow-sm  hover:bg-gray-50"
           >
             <SiGmail className="w-5 h-5" />
           </a>
@@ -35,19 +35,19 @@ const Socials = () => {
           <a
             href="https://www.linkedin.com/in/ericksonsernero777/"
             target="_blank"
-            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200  shadow-sm dark:text-gray-400 hover:bg-gray-50"
+            className="flex cursor-pointer justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200   shadow-sm  hover:bg-gray-50"
           >
             <FaLinkedinIn className="w-5 h-5" />
           </a>
         </div>
-        <Flip delay={5000} triggerOnce={false}>
+        <Flip delay={10000} triggerOnce={false}>
           <button
             type="button"
             onClick={() => setShow(!show)}
             className={
               !show
-                ? "animate-text z-[99] bg-gradient-to-r from-[#f3f3f3] via-[#b4b4b4] to-[#686868] flex items-center justify-center transition-all rounded-full w-[57px] h-[57px] text-gray-500 hover:text-gray-900 bg-white border border-gray-200  shadow-sm dark:text-gray-400 hover:bg-gray-50"
-                : " flex items-center justify-center rounded-full w-[57px] h-[57px] text-gray-500 hover:text-gray-900 bg-white border transition-all border-gray-200  shadow-sm dark:text-gray-400 hover:bg-gray-50"
+                ? "animate-text z-[99] bg-gradient-to-r from-[#fff] via-[#b4b4b4] to-[#686868] flex items-center justify-center transition-all rounded-full w-[57px] h-[57px] text-gray-500 hover:text-gray-900 bg-white border border-gray-200  shadow-sm  hover:bg-gray-50"
+                : " flex items-center justify-center rounded-full w-[57px] h-[57px] text-gray-500 hover:text-gray-900 bg-white border transition-all border-gray-200  shadow-sm  hover:bg-gray-50"
             }
           >
             <BiSolidContact className="w-8 h-8" />
