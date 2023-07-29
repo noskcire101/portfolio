ed5fb8d28cebd66891110caa77b3bfda5eb1ea50
noskcire101 <esernero@gmail.com>
2023-07-30 03:17:51 +0800

    add loader

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -6,8 +6,14 @@ import Education from "../../components/education/Education";
 import Projects from "../../components/projects/Projects";
 import Socials from "../../components/socials/Socials";
 import Logos from "../../components/logos/Logos";
+import Loader from "../../components/loader/loader";
 
 const LandingPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 5000);
+  }, []);
+
   return (
     <>
       <Header />
@@ -18,6 +24,7 @@ const LandingPage = () => {
       <Education />
       <Projects />
       <Socials />
+      {isLoading && <Loader />}
     </>
   );
 };
